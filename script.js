0071dc1f137c1b3455b1a441f7fb351b4cbc1ff9
marcoesposito20055-tech <marco.esposito20055@gmail.com>
2026-02-13 19:57:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -50,26 +50,6 @@ function updateStars() {
 
 const button = document.getElementById("valentinesButton");
 
-button.addEventListener("click", () => {
-  if (button.textContent === "Click Me! ❤") {
-    button.textContent = "loading...";
-    fetch('send_mail.php')
-      .then(response => {
-        if (response.ok) {
-          button.textContent = "Check Your Email 🙃";
-        } else {
-          console.error('Failed to send email');
-          button.textContent = "Error 😞";
-        }
-      })
-      .catch(error => {
-        // Handle network errors or other issues
-        console.error('Error:', error);
-        button.textContent = "Error 😞";
-      });
-  }
-});
-
 function drawTextWithLineBreaks(lines, x, y, fontSize, lineHeight) {
     lines.forEach((line, index) => {
         context.fillText(line, x, y + index * (fontSize + lineHeight));
